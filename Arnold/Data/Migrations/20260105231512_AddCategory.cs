3a69d8bc43ec12d84e524b7e54dcb9e4f94e9948
GKostov0 <georgi.kostov0101@gmail.com>
2026-01-06 01:22:04 +0200

    add categories

---
+++ Arnold/Data/Migrations/20260105231512_AddCategory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Arnold.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Силови" },
+                    { 2, "Кардио" },
+                    { 3, "Стречинг" },
+                    { 4, "Йога" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
---
+++ Arnold/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Arnold.Data;
+
+namespace Arnold.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Task<Category> CreateAsync(Category obj);
+        public Task<Category> UpdateAsync(Category obj);
+        public Task<bool> DeleteAsync(int id);
+        public Task<Category> GetAsync(int id);
+        public Task<IEnumerable<Category>> GetAllAsync();
+    }
+}
